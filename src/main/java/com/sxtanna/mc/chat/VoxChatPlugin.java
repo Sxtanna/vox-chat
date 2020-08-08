84e3e7130e3b547da9c1ab37d3656f0014edd008
Sxtanna <ranald.taylor@gmail.com>
2020-08-08 08:05:21 -0400

    added command api

--- src/main/java/com/sxtanna/mc/chat/VoxChatPlugin.java
+++ src/main/java/com/sxtanna/mc/chat/VoxChatPlugin.java
@@ -1,5 +1,7 @@
 package com.sxtanna.mc.chat;
 
+import com.sxtanna.mc.chat.cmds.VoxChatCommandRouter;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -22,11 +24,27 @@ public final class VoxChatPlugin extends JavaPlugin
 	{
 		getServer().getServicesManager().register(VoxChat.class, api, this, ServicePriority.Normal);
 		VoxChat.INSTANCE = api;
+
+		final PluginCommand command = getCommand("voxchat");
+		if (command != null)
+		{
+			final VoxChatCommandRouter router = new VoxChatCommandRouter(this);
+
+			command.setExecutor(router);
+			command.setTabCompleter(router);
+		}
 	}
 
 	@Override
 	public void onDisable()
 	{
+		final PluginCommand command = getCommand("voxchat");
+		if (command != null)
+		{
+			command.setExecutor(null);
+			command.setTabCompleter(null);
+		}
+
 		getServer().getServicesManager().unregister(VoxChat.class, api);
 		VoxChat.INSTANCE = null;
 	}
---
+++ src/main/java/com/sxtanna/mc/chat/cmds/VoxChatCommand.java
@@ -0,0 +1,52 @@
+package com.sxtanna.mc.chat.cmds;
+
+import com.google.common.collect.ImmutableList;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+
+public abstract class VoxChatCommand
+{
+
+	@NotNull
+	@Unmodifiable
+	private final List<String> labels;
+
+
+	protected VoxChatCommand(@NotNull final String label, @NotNull final String... aliases)
+	{
+		this.labels = ImmutableList.<String>builder().add(label).add(aliases).build();
+	}
+
+
+	@NotNull
+	public final String getLabel()
+	{
+		return labels.get(0);
+	}
+
+
+	@NotNull
+	@Unmodifiable
+	public final List<String> getAllLabels()
+	{
+		return labels;
+	}
+
+	@NotNull
+	@Unmodifiable
+	public final List<String> getAltLabels()
+	{
+		return labels.subList(1, labels.size());
+	}
+
+
+	protected void evaluate(@NotNull final CommandSender sender, @NotNull final String label, @NotNull @Unmodifiable final List<String> params)
+	{}
+
+	protected void complete(@NotNull final CommandSender sender, @NotNull final String label, @NotNull @Unmodifiable final List<String> params, @NotNull final List<String> suggestions)
+	{}
+
+}
