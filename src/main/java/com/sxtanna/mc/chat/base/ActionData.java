5bf90befaab7da78611dc7ad71e0d07473bb7385
Sxtanna <ranald.taylor@gmail.com>
2020-08-09 01:33:24 -0400

    added chat action data and types

---
+++ src/main/java/com/sxtanna/mc/chat/base/ActionData.java
@@ -0,0 +1,118 @@
+package com.sxtanna.mc.chat.base;
+
+import com.sxtanna.mc.chat.util.Comp;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public abstract class ActionData
+{
+
+	@NotNull
+	private final String text;
+
+
+	ActionData(@NotNull final String text)
+	{
+		this.text = text;
+	}
+
+	@NotNull
+	@Contract(pure = true)
+	public final String getText()
+	{
+		return text;
+	}
+
+
+	@NotNull
+	public Optional<ClickEvent> getClickEvent()
+	{
+		return Optional.empty();
+	}
+
+	@NotNull
+	public Optional<HoverEvent> getHoverEvent()
+	{
+		return Optional.empty();
+	}
+
+
+	public static final class ActionDataHover extends ActionData
+	{
+
+		private ActionDataHover(@NotNull final String text)
+		{
+			super(text);
+		}
+
+
+		@NotNull
+		@Override
+		public Optional<HoverEvent> getHoverEvent()
+		{
+			return Optional.of(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(Comp.of(getText()))));
+		}
+
+	}
+
+	public static final class ChatActionClickExecCmd extends ActionData
+	{
+
+		private ChatActionClickExecCmd(@NotNull final String text)
+		{
+			super(text);
+		}
+
+		@Override
+		public @NotNull Optional<ClickEvent> getClickEvent()
+		{
+			return Optional.of(new ClickEvent(ClickEvent.Action.RUN_COMMAND, getText()));
+		}
+
+	}
+
+	public static final class ChatActionClickShowCmd extends ActionData
+	{
+
+		private ChatActionClickShowCmd(@NotNull final String text)
+		{
+			super(text);
+		}
+
+		@Override
+		public @NotNull Optional<ClickEvent> getClickEvent()
+		{
+			return Optional.of(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, getText()));
+		}
+
+	}
+
+	public static final class ChatActionClickOpenUrl extends ActionData
+	{
+
+		private ChatActionClickOpenUrl(@NotNull final String text)
+		{
+			super(text);
+		}
+
+		@Override
+		public @NotNull Optional<ClickEvent> getClickEvent()
+		{
+			return Optional.of(new ClickEvent(ClickEvent.Action.OPEN_URL, getText()));
+		}
+
+	}
+
+
+	@NotNull
+	public static ActionData hover(@NotNull final String text)
+	{
+		return new ActionDataHover(text);
+	}
+
+}
---
+++ src/main/java/com/sxtanna/mc/chat/base/ActionType.java
@@ -0,0 +1,25 @@
+package com.sxtanna.mc.chat.base;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public enum ActionType
+{
+	HOVER,
+	CLICK;
+
+	@NotNull
+	public static Optional<ActionType> find(@NotNull final String name)
+	{
+		switch (name.toLowerCase())
+		{
+			case "hover":
+				return Optional.of(HOVER);
+			case "click":
+				return Optional.of(CLICK);
+			default:
+				return Optional.empty();
+		}
+	}
+}
