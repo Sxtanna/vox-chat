8d8ad5c3dabc7736438dbc74fcacdf34a3d7a439
Sxtanna <ranald.taylor@gmail.com>
2020-08-09 01:32:42 -0400

    added component utility

---
+++ src/main/java/com/sxtanna/mc/chat/util/Comp.java
@@ -0,0 +1,120 @@
+package com.sxtanna.mc.chat.util;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class Comp
+{
+
+	private static final Pattern HEX_COLOR = Pattern.compile("&#(?<hex>[0-9a-fA-F]{6})");
+
+	private Comp()
+	{}
+
+
+	@NotNull
+	public static BaseComponent[] of(@NotNull final String text)
+	{
+		final Matcher       matcher = HEX_COLOR.matcher(text);
+		final StringBuilder builder = new StringBuilder(text);
+		while (matcher.find())
+		{
+			//noinspection SuspiciousRegexArgument
+			builder.replace(matcher.start(), matcher.end(), "§x" + matcher.group("hex").replaceAll(".", "§$0"));
+		}
+
+		return TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', builder.toString()));
+	}
+
+	@NotNull
+	public static String to(@NotNull final BaseComponent[] json)
+	{
+		return ComponentSerializer.toString(json);
+	}
+
+
+	@NotNull
+	@Contract(value = " -> new", pure = true)
+	public static ComponentBuilder build()
+	{
+		return build("");
+	}
+
+	@NotNull
+	@Contract(value = "_ -> new", pure = true)
+	public static ComponentBuilder build(@NotNull final String text)
+	{
+		return new ComponentBuilder(text);
+	}
+
+	@NotNull
+	@Contract(value = "_ -> new", pure = true)
+	public static ComponentBuilder build(@NotNull final BaseComponent[] components)
+	{
+		return new ComponentBuilder(new TextComponent(components));
+	}
+
+
+	@NotNull
+	@Contract("_ -> param1")
+	public static ComponentBuilder clean(@NotNull final ComponentBuilder builder)
+	{
+		final Iterator<BaseComponent> iterator = builder.getParts().iterator();
+		while (iterator.hasNext())
+		{
+			final BaseComponent next = iterator.next();
+			if (!(next instanceof TextComponent))
+			{
+				continue;
+			}
+
+			final TextComponent text = (TextComponent) next;
+			if ((text.getText() == null || text.getText().isEmpty()) && (text.getExtra() == null || text.getExtra().isEmpty()))
+			{
+				iterator.remove();
+			}
+		}
+
+		return builder;
+	}
+
+
+	public static void walk(@NotNull final BaseComponent[] components, @NotNull final ComponentVisitor visitor)
+	{
+		Arrays.stream(components).forEach(component -> walk(component, visitor));
+	}
+
+	private static void walk(@NotNull final BaseComponent component, @NotNull final ComponentVisitor visitor)
+	{
+		visitor.visit(component);
+
+		final List<BaseComponent> extra = component.getExtra();
+		if (extra == null || extra.isEmpty())
+		{
+			return;
+		}
+
+		extra.forEach(other -> walk(other, visitor));
+	}
+
+
+	@FunctionalInterface
+	public interface ComponentVisitor
+	{
+
+		void visit(@NotNull final BaseComponent component);
+
+	}
+
+}
