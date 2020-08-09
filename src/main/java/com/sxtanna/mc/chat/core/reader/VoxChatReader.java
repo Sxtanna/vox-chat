847d60dcec06f1ccf976246c10728d6d0ec7f21a
Sxtanna <ranald.taylor@gmail.com>
2020-08-09 01:58:35 -0400

    added higher level markdown reader

---
+++ src/main/java/com/sxtanna/mc/chat/core/reader/VoxChatReader.java
@@ -0,0 +1,33 @@
+package com.sxtanna.mc.chat.core.reader;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.Optional;
+
+public final class VoxChatReader
+{
+
+	@NotNull
+	private final Parser parser = Parser.builder()
+										.enabledBlockTypes(Collections.emptySet())
+										.build();
+
+
+	@NotNull
+	public Optional<Node> read(@NotNull @Language("Markdown") final String text)
+	{
+		try
+		{
+			return Optional.ofNullable(parser.parse(text));
+		}
+		catch (final Exception ex)
+		{
+			return Optional.empty();
+		}
+	}
+
+}
