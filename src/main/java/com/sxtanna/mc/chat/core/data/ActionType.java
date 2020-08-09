c17626ae8c1f466a9a44c677d1f10eb692966ef0
Sxtanna <ranald.taylor@gmail.com>
2020-08-09 01:56:33 -0400

    updated actiontype to read data from configs

--- src/main/java/com/sxtanna/mc/chat/core/data/ActionType.java
+++ src/main/java/com/sxtanna/mc/chat/core/data/ActionType.java
@@ -1,13 +1,72 @@
 package com.sxtanna.mc.chat.core.data;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
+import java.util.Set;
 
 public enum ActionType
 {
-	HOVER,
-	CLICK;
+	HOVER
+			{
+				@NotNull
+				@Override
+				public Optional<ActionData> read(@NotNull final ConfigurationSection section)
+				{
+					final String text;
+
+					if (!section.isList("text"))
+					{
+						text = section.getString("text");
+					}
+					else
+					{
+						text = String.join("\n", section.getStringList("text"));
+					}
+
+
+					return text == null ? Optional.empty() : Optional.of(ActionData.hover(text));
+				}
+			},
+	CLICK
+			{
+				@NotNull
+				@Override
+				public Optional<ActionData> read(@NotNull final ConfigurationSection section)
+				{
+					final Set<String> keys = section.getKeys(false);
+					if (keys.size() != 1)
+					{
+						return Optional.empty();
+					}
+
+					final String k = keys.iterator().next();
+
+					final String v = section.getString(k);
+					if (v == null || v.isEmpty())
+					{
+						return Optional.empty();
+					}
+
+					switch (k.toLowerCase())
+					{
+						case "exec_cmd":
+							return Optional.of(ActionData.execCmd(v));
+						case "show_cmd":
+							return Optional.of(ActionData.showCmd(v));
+						case "open_url":
+							return Optional.of(ActionData.openUrl(v));
+						default:
+							return Optional.empty();
+					}
+				}
+			};
+
+
+	@NotNull
+	public abstract Optional<ActionData> read(@NotNull final ConfigurationSection section);
+
 
 	@NotNull
 	public static Optional<ActionType> find(@NotNull final String name)
