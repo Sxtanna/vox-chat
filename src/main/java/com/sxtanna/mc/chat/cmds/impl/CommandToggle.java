332a2250884339c027a9d52af659cbe26ef37b51
Sxtanna <ranald.taylor@gmail.com>
2020-08-10 03:19:06 -0400

    implemented toggle command

--- src/main/java/com/sxtanna/mc/chat/cmds/impl/CommandToggle.java
+++ src/main/java/com/sxtanna/mc/chat/cmds/impl/CommandToggle.java
@@ -1,11 +1,16 @@
 package com.sxtanna.mc.chat.cmds.impl;
 
 import com.sxtanna.mc.chat.cmds.VoxChatCommand;
+import com.sxtanna.mc.chat.core.events.ChatListener;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.List;
+
+import static net.md_5.bungee.api.ChatColor.GRAY;
+import static net.md_5.bungee.api.ChatColor.GREEN;
+import static net.md_5.bungee.api.ChatColor.RED;
 
 public final class CommandToggle extends VoxChatCommand
 {
@@ -19,7 +24,50 @@ public final class CommandToggle extends VoxChatCommand
 	@Override
 	protected void evaluate(@NotNull final CommandSender sender, @NotNull final String label, @NotNull @Unmodifiable final List<String> params)
 	{
+		final ChatListener listener = getPlugin().getListener();
 
+		final boolean desiredState;
+		final boolean currentState = listener.isLoaded();
+
+		switch (label.toLowerCase())
+		{
+			case "enable":
+				desiredState = true;
+				break;
+			case "disable":
+				desiredState = false;
+				break;
+			default:
+				desiredState = !currentState;
+				break;
+		}
+
+		if (currentState == desiredState)
+		{
+
+			sender.spigot().sendMessage(prefix().append(" is already ")
+												.color(GRAY)
+												.append(currentState ? "enabled" : "disabled")
+												.color(currentState ? GREEN : RED)
+												.create());
+
+			return;
+		}
+
+		if (desiredState)
+		{
+			listener.load();
+		}
+		else
+		{
+			listener.kill();
+		}
+
+		sender.spigot().sendMessage(prefix().append(" is now ")
+											.color(GRAY)
+											.append(desiredState ? "enabled" : "disabled")
+											.color(desiredState ? GREEN : RED)
+											.create());
 	}
 
 }
