1008063503d130849d8ca9451c30680685125260
Sxtanna <ranald.taylor@gmail.com>
2020-08-10 03:17:45 -0400

    added command prefix method

--- src/main/java/com/sxtanna/mc/chat/cmds/VoxChatCommand.java
+++ src/main/java/com/sxtanna/mc/chat/cmds/VoxChatCommand.java
@@ -2,11 +2,20 @@ package com.sxtanna.mc.chat.cmds;
 
 import com.google.common.collect.ImmutableList;
 import com.sxtanna.mc.chat.VoxChatPlugin;
+import com.sxtanna.mc.chat.util.Comp;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.List;
+
+import static net.md_5.bungee.api.ChatColor.DARK_GRAY;
+import static net.md_5.bungee.api.ChatColor.WHITE;
+import static net.md_5.bungee.api.ChatColor.YELLOW;
 
 public abstract class VoxChatCommand
 {
@@ -62,6 +71,39 @@ public abstract class VoxChatCommand
 	final void setPlugin(@NotNull final VoxChatPlugin plugin)
 	{
 		this.plugin = plugin;
+	}
+
+
+	@NotNull
+	protected final ComponentBuilder prefix()
+	{
+		return Comp.build()
+				   .append("::")
+				   .color(WHITE)
+				   .bold(true)
+
+				   .append(" VoxChat ")
+				   .color(DARK_GRAY)
+				   .bold(true)
+
+				   .event(new ClickEvent(ClickEvent.Action.OPEN_URL, getPlugin().getDescription().getWebsite()))
+				   .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(Comp.build()
+																				   .append("v")
+																				   .color(DARK_GRAY)
+																				   .bold(false)
+																				   .append(getPlugin().getDescription().getVersion())
+																				   .color(WHITE)
+																				   .append("\n")
+																				   .append("  - Sxtanna")
+																				   .color(YELLOW)
+																				   .create())))
+
+				   .append("::")
+				   .color(WHITE)
+				   .bold(true)
+
+				   .append("")
+				   .retain(ComponentBuilder.FormatRetention.NONE);
 	}
 
 }
