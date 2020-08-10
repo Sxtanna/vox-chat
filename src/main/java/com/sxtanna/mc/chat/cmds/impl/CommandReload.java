b2c6c7c85884e30d525131a765693af980598e65
Sxtanna <ranald.taylor@gmail.com>
2020-08-10 03:18:05 -0400

    implemented reload command

--- src/main/java/com/sxtanna/mc/chat/cmds/impl/CommandReload.java
+++ src/main/java/com/sxtanna/mc/chat/cmds/impl/CommandReload.java
@@ -6,6 +6,13 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+
+import static net.md_5.bungee.api.ChatColor.DARK_GRAY;
+import static net.md_5.bungee.api.ChatColor.GOLD;
+import static net.md_5.bungee.api.ChatColor.GREEN;
+import static net.md_5.bungee.api.ChatColor.RED;
 
 public final class CommandReload extends VoxChatCommand
 {
@@ -19,7 +26,27 @@ public final class CommandReload extends VoxChatCommand
 	@Override
 	protected void evaluate(@NotNull final CommandSender sender, @NotNull final String label, @NotNull @Unmodifiable final List<String> params)
 	{
+		final Optional<Throwable> result = getPlugin().reloadPlugin();
+		if (!result.isPresent())
+		{
 
+			sender.spigot().sendMessage(prefix().append(" successfully reloaded.")
+												.color(GREEN)
+												.create());
+
+			return;
+		}
+
+		sender.spigot().sendMessage(prefix().append(" failed to reload")
+											.color(RED)
+											.append(":")
+											.color(DARK_GRAY)
+											.append("\n  ")
+											.append(result.get().getMessage())
+											.color(GOLD)
+											.create());
+
+		getPlugin().getLogger().log(Level.SEVERE, "failed to reload plugin", result.get());
 	}
 
 }
